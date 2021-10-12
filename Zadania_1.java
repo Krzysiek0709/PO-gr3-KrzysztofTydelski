d08ec5ea79225dab37d4e1ff859a5137362905aa
Krzysiek0709 <76214275+Krzysiek0709@users.noreply.github.com>
2021-10-12 11:01:26 +0200

    Add files via upload

---
+++ Zadania_1.java
@@ -0,0 +1,103 @@
+public class Zadania_1 {
+
+    public static void main(String[] args) {
+        //zadanie 1
+        System.out.println(31+29+31);
+
+
+        //zadanie 2
+        int suma=0;
+        for (int i=0;i<=10;i++)
+        {
+            suma+=i;
+        }
+        System.out.println(suma);
+
+
+        //zadanie 3
+        int il=1;
+        for (int i=1;i<=10;i++)
+        {
+            il*=i;
+        }
+        System.out.println(il);
+
+
+        //zadanie 4
+        float saldoKonta=3000;
+        for(int i=1;i<=3;i++)
+        {
+            saldoKonta+=saldoKonta*0.06;
+            System.out.println(i + " rok = "+saldoKonta);
+        }
+
+
+        //zadanie 5
+        System.out.println(" +----+\n |JAVA|\n +----+");
+
+
+        //zadanie 6
+        System.out.println("   /////    \n  +\"\"\"\"\"+  \n (| o o |)\n   | ^ |   \n  | `-' |  \n  +-----+  \n");
+
+
+        //zadanie 7
+
+        System.out.println("*    *    * *      * * * * *     *       *      * * *      *    * * * *    *    *       ");
+        System.out.println("*   *     *   *            *       *   *       *     *          *          *   *       ");
+        System.out.println("*  *      *   *          *           *          *          *    *          *  *     ");
+        System.out.println("* *       * *          *             *            *        *    * * * *    * *       ");
+        System.out.println("*  *      * *        *               *              *      *    *          *  *      ");
+        System.out.println("*   *     *  *     *                 *          *     *    *    *          *   *         ");
+        System.out.println("*    *    *   *    * * * * *         *           * * *     *    * * * *    *    *      ");
+
+
+        //zadanie 8
+        System.out.println("   +    \n  + +   \n +   +  \n+-----+\n| .-. |\n| | | |\n+-+-+-+\n");
+
+
+        //zadanie 9
+        System.out.println(
+                "    (0v0)            -----\n" +
+                "   __ | __         / Hello\\\n" +
+                "  |   |   |       < Junior |\n" +
+                "    __|__          \\Coder!/\n" +
+                "   |     |           -----");
+
+
+        //zadanie 10
+        System.out.println(
+                "1.Maćko\n" +
+                "2.Husarz\n" +
+                "3.Porko");
+
+
+        //zadanie 11
+        System.out.println("Dłoń\n" +
+                "Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.");
+
+
+        //zadanie 12
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println(" * * * * *  ===========================================");
+        System.out.println("* * * * * * ===========================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+        System.out.println("=======================================================");
+    }
+
+}

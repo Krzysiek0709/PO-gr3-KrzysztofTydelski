aff8c884c10298f8fcfea9205c02950dfb8ab5d2
Krzysiek0709 <76214275+Krzysiek0709@users.noreply.github.com>
2022-01-19 12:05:08 +0100

    Create Rekrutacja.java

---
+++ Test-2/Rekrutacja.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class Rekrutacja {
+    static int doswiadczenie;
+    private ArrayList<Kandydet> kandydatArrayList;
+
+    public static void setDoswiadczenie(int doswiadczenie){
+        Rekrutacja.doswiadczenie=2;
+    }
+    public static int getDoswiadczenie() {
+        return doswiadczenie;
+    }
+}
